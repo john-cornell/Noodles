a4fd18380cb5fd579f02465f03167d19c238e5e2
ghastly bluff <forgithub@ghastlybluff.com>
2021-05-27 21:21:45 +1000

    GeometricMean

--- Noodles/Mean.cs
+++ Noodles/Mean.cs
@@ -1,15 +1,24 @@
 ﻿using Noodles.MeansCalculations;
 using System;
+using System.Collections.Generic;
 
 namespace Noodles
 {
     public class Mean
     {
-        public ArithmeticMeanCalculator Arithmetic { get; private set; }
-
+        ArithmeticMeanCalculator _arithmetic { get; set; }
+        GeometricMeanCalculator _geometric { get; set; }
         public Mean()
         {
-            Arithmetic = new ArithmeticMeanCalculator();
+            _arithmetic = new ArithmeticMeanCalculator();
+            _geometric = new GeometricMeanCalculator();
+
         }
+
+        public double Arithmetic(IEnumerable<int> values) => _arithmetic.Calculate(values);
+        public double Arithmetic(IEnumerable<double> values) => _arithmetic.Calculate(values);
+
+        public double Geometric(IEnumerable<int> values) => _geometric.Calculate(values);
+        public double Geometric(IEnumerable<double> values) => _geometric.Calculate(values);
     }
 }
